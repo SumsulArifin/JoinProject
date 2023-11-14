dbed8ba7bef15d28a97aeb3eaf795fc0666871b8
Md.Sumsul Arifin <99475443+SumsulArifin@users.noreply.github.com>
2023-11-14 12:58:21 +0600

    ss
    ss

--- Arifin/JWT-Authentication-and-Authorization-in-Spring-Security---Spring-Boot-REST-API-main/JWT-with-Spring-Security/src/main/java/com/spring/jwt/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.spring.jwt.service;
-
-import com.spring.jwt.dto.UserDTO;
-import com.spring.jwt.utils.BaseResponseDTO;
-
-public interface UserService {
-    BaseResponseDTO registerAccount(UserDTO userDTO);
-}
